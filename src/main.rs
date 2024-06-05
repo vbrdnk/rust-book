8fcab259f92f0ffc541f50668b93bac02c58c9ea
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-06-04 17:58:30 -0700

    Rectangles project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let square = Rectangle::square(45);
+    println!("The area of square rectangle is {}", square.area());
+    println!("The area of the rectangle is {}", rect1.area());
+    println!("rect1 is {:#?}", rect1);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect2 hold rect3? {}", rect2.can_hold(&rect3));
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.area() > other.area()
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
