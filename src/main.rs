e6b3169364aefdbe72d95ddeef99efd81d4081fa
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-06-04 21:57:44 -0700

    enums impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
     println!("Hello, world!");
+
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
+
+    let write_message = Message::Write(String::from("hello"));
+    write_message.call();
+
 }
+
+enum IpAddrKind {
+    V4(String),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+
+    }
+}
+
+fn route(ip_kind: IpAddrKind) {}
