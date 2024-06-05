e1482b1b2c8f0d967059f96eaedb93c7dfcccbac
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-06-04 22:25:10 -0700

    Control flow with match

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("Penny is {} cent", value_in_cents(Coin::Penny));
+    println!("Quarter is {} cents", value_in_cents(Coin::Quarter(UsState::Alabama)));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let config_max = Some(3u8);
+
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1)
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
 }
