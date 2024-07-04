cf111ca459e67644cf5217b68dcc0059ad8499e7
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-07-04 14:21:31 -0700

    Iterators and closures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got value: {val}");
+    }
+
+    let v2 = vec![1, 2, 3];
+    let v2_mapped: Vec<i32> = v2.iter().map(|x| x * 2).collect();
+    assert_eq!(v2_mapped, vec![2, 4, 6]);
 }
