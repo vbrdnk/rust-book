3702cbf9d6fd6481a62271a869413a6f95b07407
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-08-02 21:17:09 -0700

    Learning smart pointers, deref trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::ops::Deref;
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
 }
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(x, 5);
+    assert_eq!(*y, 5);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
