3168051b6ff08b74cece40d949f1313397dc2285
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-08-28 22:11:53 -0700

    OOP rust way

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rust_book::Post;
+
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
