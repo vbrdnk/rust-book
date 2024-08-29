13a2a591af6f814cae3df20462d21205f145538d
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-08-28 17:54:22 -0700

    Use trait objects to implement inheritance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rust_book::{Draw, Screen, Button};
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("Drawing a select box with options {:?}", self.options);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ]
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("Click me!"),
+            }),
+            Box::new(String::from("Hello, world!")), // error: the trait `Draw` is not implemented for `std::string::String`
+        ]
+    };
+
+    screen.run();
 }
